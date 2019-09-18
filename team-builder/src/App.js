339ecf8d682bbf87040e717907102ae5b6567d1a
David Kuseh <davegru203@gmail.com>
2019-09-18 14:45:57 +0000

    created form and initial team list

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,25 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import uuid from 'uuid';
 import './App.css';
 
-function App() {
+const initialTeamMembers = [
+  {id: uuid(), name: 'Martin', role: 'Team Lead'},
+  {id: uuid(), name: 'Temi', role: 'FSW Student'},
+  {id: uuid(), name: 'Justin', role: 'FSW Student'},
+  {id: uuid(), name: 'Femi', role: 'FSW Student'},
+  {id: uuid(), name: 'Alison', role: 'FSW Student'},
+  {id: uuid(), name: 'David', role: 'FSW Student'},
+
+];
+
+const initialFormValues = {
+  name: '',
+  role: '',
+}
+
+function TeamMember({ teamMember }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>{teamMember.name}</span>
+      <span>{teamMember.role}</span>
     </div>
+  );
+}
+
+function TeamList({ teamMembers }) {
+  return (
+    <>
+      <h2>Team List</h2>
+      <div>
+        <span>Name</span>
+        <span>Role</span>
+      </div>
+      {teamMembers.map(teamMember => <TeamMember teamMember={ teamMember } />)}
+    </>
+  );
+} 
+
+function Form({formValues, handleChange, onSubmit, isDisabled}) {
+  return (
+    <>
+      <h2>Add Team Member</h2>
+      <form>
+        <div>
+          <label htmlFor='name'>Name</label>
+          <input value={formValues.name} onChange={handleChange} type='text' id='name' />
+        </div>
+        <div>
+          <label htmlFor='title'>Role</label>
+          <input value={formValues.role} onChange={handleChange} type='text' id='role' />
+        </div>
+        <button onClick={onSubmit} disabled={isDisabled()} >Submit</button>
+      </form>
+    </>
+  );
+}
+
+function App() {
+  
+  const [teamMembers, setTeamMembers] = useState(initialTeamMembers);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.id]: e.target.value })
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setTeamMembers( [...teamMembers, formValues ]);
+    setFormValues(initialFormValues);
+  }
+
+  const isDisabled = () => !(formValues.name &&  formValues.role);
+
+  return (
+    <>
+      <div className="App">
+        <div className='form'>
+          <Form 
+            formValues={formValues}
+            handleChange={handleChange}
+            onSubmit={onSubmit}
+            isDisabled={isDisabled}
+          />
+        </div>
+        <div className='team'>
+          <TeamList teamMembers={ teamMembers } />
+        </div>
+      </div>
+    </>
   );
 }
 
